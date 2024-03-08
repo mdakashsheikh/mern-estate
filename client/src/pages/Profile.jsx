642ef20d310af89fd8ccbce12c1cc7099142734d
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-03-09 01:27:50 +0600

    last of 08 march

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -88,6 +88,14 @@ const Profile = () => {
         }
     }
 
+    const handleSignOut = async() => {
+        try {
+            const res = await axios.get('/api/auth/signout')
+        } catch (error) {
+            
+        }
+    }
+
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -142,7 +150,7 @@ const Profile = () => {
             </form>
             <div className="flex justify-between mt-5">
                 <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer ">Delete Account</span>
-                <span className="text-red-700 cursor-pointer ">Sign Out</span>
+                <span onClick={handleSignOut} className="text-red-700 cursor-pointer ">Sign Out</span>
             </div>
             <p className="text-red-700 mt-5">{error ? error : ''}</p>
             <p className="text-green-700 mt-5">{updateSuccess ? 'User is updated successfully!' : ''}</p>
